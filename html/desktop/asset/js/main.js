58546f25c9f7468fdc6e7945923c6c122d2f3355
Khoirul Huda <mhkhoirulhuda@gmail.com>
2019-12-23 17:08:41 +0700

    revisi 1.3

--- html/desktop/asset/js/main.js
+++ html/desktop/asset/js/main.js
@@ -151,5 +151,23 @@ $(document).ready(function() {
 		e.preventDefault();
 		$('.ads__horizontal').hide();
 	});
+
+	// kompetisi
+	$('.competition__item').each(function (){
+		var list = $(this).find('.competition__sublist').outerHeight();
+		var hbtn = $(this).find('.competition__title').outerHeight();
+		$(this).find('.competition__title').on('click', function(e){
+			e.preventDefault();
+			var pr = $(this).parent();
+			//console.log(pr);
+			if(pr.hasClass('active')){
+				pr.removeAttr('style');
+				pr.removeClass('active');
+			} else {
+				pr.toggleClass('active');
+				pr.css('height', (list + hbtn));
+			}
+		})
+	})
 	
 });
